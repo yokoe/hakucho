a16d9b008b4c4cbef80053d9f5e7a150cb1500e2
Sota Yokoe <kreuz45@kreuz45.com>
2022-10-12 10:34:49 +0900

    feat: CreateSubFolder

--- folder.go
+++ folder.go
@@ -3,6 +3,19 @@ package hakucho
 import "google.golang.org/api/drive/v3"
 
 // CreateFolder creates new folder
-func (srv *Client) CreateFolder(folderName string) (*drive.File, error) {
-	return srv.driveService.Files.Create(&drive.File{Name: folderName, MimeType: "application/vnd.google-apps.folder"}).Do()
+func (c *Client) CreateFolder(folderName string) (*drive.File, error) {
+	return c.driveService.Files.Create(&drive.File{Name: folderName, MimeType: "application/vnd.google-apps.folder"}).Do()
+}
+
+// CreateSubFolder creates new folder and set parent folder id of it
+func (c *Client) CreateSubFolder(parentFolderID string, folderName string) (*drive.File, error) {
+	folder, err := c.CreateFolder(folderName)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := c.driveService.Files.Update(folder.Id, &drive.File{}).AddParents(parentFolderID).Do(); err != nil {
+		return nil, err
+	}
+	return folder, nil
 }
--- folder_test.go
+++ folder_test.go
@@ -18,3 +18,24 @@ func TestCreateFolder(t *testing.T) {
 		t.Errorf("Failed to create folder %s: %s", folderName, err)
 	}
 }
+
+func TestCreateSubFolder(t *testing.T) {
+	c, err := newTestClient(t)
+	if err != nil {
+		return
+	}
+
+	dateStr := time.Now().Format("20060102_150406")
+	folderName := fmt.Sprintf("hakucho-test-parent-%s", dateStr)
+	parent, err := c.CreateFolder(folderName)
+	if err != nil {
+		t.Errorf("Failed to create folder %s: %s", folderName, err)
+	}
+
+	subFolderName := fmt.Sprintf("hakucho-test-child-%s", dateStr)
+	_, err = c.CreateSubFolder(parent.Id, subFolderName)
+	if err != nil {
+		t.Errorf("Failed to create sub folder %s: %s", subFolderName, err)
+	}
+
+}
